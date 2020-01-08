a3dcd61b9d406f4c7fb01465461d3486836ed201
Curtis <Jamesaddy789@gmail.com>
2020-01-08 15:48:24 -0600

    Added instruction file and reorganized files. Removed uneccessary files and renamed scripts

--- Mbrace Python Class/RegexDatesTest.py
+++
@@ -1,7 +0,0 @@
-import re
-
-name = 'Composite_24_6F_28_A0_DA_30_2019-12-16_to_24_6F_28_A0_DA_30_2020-01-25.bin'
-date_match_list = re.findall(r"\d{4}-\d{2}-\d{2}", name)
-for date in date_match_list:
-    print(date)
-
