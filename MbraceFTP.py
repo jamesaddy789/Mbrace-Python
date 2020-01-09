7ee147b73a12c8f7fa989d1fa8caa929d1bdff1b
Curtis <Jamesaddy789@gmail.com>
2020-01-08 18:48:32 -0600

    Made it so the user can input the directory to plot files from. Added instructions

--- MbraceFTP.py
+++ MbraceFTP.py
@@ -2,6 +2,7 @@
 
 import ftplib
 import re
+import os
 
 def mbrace_ftp_connect():
     try:
@@ -28,21 +29,26 @@ def list_all():
         print(sort_dict[key])
         
 def operate_file_download():
+    download_directory = "Mbrace Data Files"
+    if not os.path.exists(download_directory):
+        os.mkdir(download_directory)
+        print("Directory " + download_directory + " created.")
     file_to_download = input("\nCopy and paste name of file to download :")
-    local_name = file_to_download.replace(':', '_')    
-    local_file = open(local_name, 'wb')
+    local_name = file_to_download.replace(':', '_')
+    local_file_path = download_directory + "/" + local_name
+    local_file = open(local_file_path, 'wb')
     ftp = mbrace_ftp_connect()
     if ftp != None:
         ftp.retrbinary("RETR " + file_to_download ,local_file.write)
         local_file.close()
         ftp.quit()
-    print("Downloaded " + file_to_download + " to: \n" + local_name)
-    
+    print("Downloaded " + file_to_download + " to: \n" + local_file_path)
+
 while True:
     list_all()
     operate_file_download()   
-    restart = input("Enter c/C to restart:")
-    if restart is not "c" and restart is not "C": break
+    restart = input("Enter r/R to restart:")
+    if restart is not "r" and restart is not "R": break
 
 
 
--- MbracePlot.py
+++ MbracePlot.py
@@ -5,69 +5,103 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import datetime
+import os
 from matplotlib.widgets import RadioButtons
 
-#Get data from user specified file
-filename = input("Enter file name in current folder : ")
+def list_all_data_files(directory_name):    
+    directory_list = os.listdir(directory_name)
+    sort_dict = {}
+    file_count = 0
+    for file in directory_list:   
+        if file.endswith('.bin'):
+            date = re.search(r"\d{4}-\d{2}-\d{2}", file)           
+            if date == None : continue
+            sort_dict[date.group()] = file
+            file_count += 1
+    if file_count > 0:
+        print("\nList of all data files in selected directory:")
+        for key in sorted(sort_dict.keys()):
+            print(sort_dict[key])
+    return file_count
 
-file_date_string = re.findall(r"\d{4}-\d{2}-\d{2}", filename)[0]
-file_datetime = datetime.datetime.strptime(file_date_string, '%Y-%m-%d')
-mb_data = Mbrace.Mbrace_Data(filename)
+def run_plot_program(directory_name):
+    #Get data from user specified file
+    filename = input("\nCopy and paste the file to plot : ")
 
-#Get server timestamps in seconds
-server_timestamps = mb_data.Server_Timestamps
-hour_to_seconds = server_timestamps[:, 0] * 3600.0
-min_to_seconds = server_timestamps[:, 1] * 60.0
-seconds = hour_to_seconds + min_to_seconds + server_timestamps[:, 2]
+    file_date_string = re.findall(r"\d{4}-\d{2}-\d{2}", filename)[0]
+    file_datetime = datetime.datetime.strptime(file_date_string, '%Y-%m-%d')
+    mb_data = Mbrace.Mbrace_Data(os.path.join(directory_name,filename))
 
-#Create a matrix filled with NaN
-readingsVStime = np.full((86400, 6),np.nan)
+    #Get server timestamps in seconds
+    server_timestamps = mb_data.Server_Timestamps
+    hour_to_seconds = server_timestamps[:, 0] * 3600.0
+    min_to_seconds = server_timestamps[:, 1] * 60.0
+    seconds = hour_to_seconds + min_to_seconds + server_timestamps[:, 2]
 
-#For every second in the server timestamps,
-#put the readings at that second into the
-#readingsVStime dataset
-for i in range(seconds.shape[0]):
-    row_number = int(seconds[i])  
-    readings = mb_data.Gape_Readings[i, :]
-    readingsVStime[row_number,:] = readings
+    #Create a matrix filled with NaN
+    readingsVStime = np.full((86400, 6),np.nan)
 
-### create data
-time_axis = [file_datetime + datetime.timedelta(seconds=i) for i in range(len(readingsVStime))]
+    #For every second in the server timestamps,
+    #put the readings at that second into the
+    #readingsVStime dataset
+    for i in range(seconds.shape[0]):
+        row_number = int(seconds[i])  
+        readings = mb_data.Gape_Readings[i, :]
+        readingsVStime[row_number,:] = readings
 
-#Setup plot lines
-fig, ax = plt.subplots()
-l1, = ax.plot(time_axis, readingsVStime[:,0], color='y')
-l2, = ax.plot(time_axis, readingsVStime[:,1], visible = False, color='r')
-l3, = ax.plot(time_axis, readingsVStime[:,2], visible = False, color='g')
-l4, = ax.plot(time_axis, readingsVStime[:,3], visible = False, color='b')
-l5, = ax.plot(time_axis, readingsVStime[:,4], visible = False, color='m')
-l6, = ax.plot(time_axis, readingsVStime[:,5], visible = False, color='c')
+    ### create data
+    time_axis = [file_datetime + datetime.timedelta(seconds=i) for i in range(len(readingsVStime))]
 
-lines = [l1,l2,l3,l4,l5,l6]
+    #Setup plot lines
+    fig, ax = plt.subplots()
+    l1, = ax.plot(time_axis, readingsVStime[:,0], color='y')
+    l2, = ax.plot(time_axis, readingsVStime[:,1], visible = False, color='r')
+    l3, = ax.plot(time_axis, readingsVStime[:,2], visible = False, color='g')
+    l4, = ax.plot(time_axis, readingsVStime[:,3], visible = False, color='b')
+    l5, = ax.plot(time_axis, readingsVStime[:,4], visible = False, color='m')
+    l6, = ax.plot(time_axis, readingsVStime[:,5], visible = False, color='c')
 
-#Set title and axis labels
-plt.title("Gape Readings vs. Time\n" + file_date_string)
-plt.ylabel("Gape Readings")
-plt.xlabel("Time")
-plt.gcf().autofmt_xdate()
+    lines = [l1,l2,l3,l4,l5,l6]
 
-#Setup radio buttons
-box_color = 'lightgoldenrodyellow'
-radio_ax = plt.axes([.9, .2, .2, .2], facecolor = box_color)
-radio = RadioButtons(radio_ax, ('1','2','3','4','5','6'))
-label_dict = {'1':0, '2':1, '3':2, '4':3, '5':4, '6':5}
+    #Set title and axis labels
+    plt.title("Gape Readings vs. Time\n" + file_date_string)
+    plt.ylabel("Gape Readings")
+    plt.xlabel("Time")
+    plt.gcf().autofmt_xdate()
 
-def set_visible_sensor(label):
-    index = label_dict[label]
-    for line in lines:
-        line.set_visible(False)
-    lines[index].set_visible(True)
-    plt.draw()
-    
-radio.on_clicked(set_visible_sensor)
+    #Setup radio buttons
+    box_color = 'lightgoldenrodyellow'
+    radio_ax = plt.axes([.9, .2, .2, .2], facecolor = box_color)
+    radio = RadioButtons(radio_ax, ('1','2','3','4','5','6'))
+    label_dict = {'1':0, '2':1, '3':2, '4':3, '5':4, '6':5}
 
-#Show plot
-plt.show()
+    def set_visible_sensor(label):
+        index = label_dict[label]
+        for line in lines:
+            line.set_visible(False)
+        lines[index].set_visible(True)
+        plt.draw()
+        
+    radio.on_clicked(set_visible_sensor)
 
+    #Show plot
+    plt.show()
 
+while True:
+    print("List of subdirectories in current directory:")
+    ignore_list = [".git", "__pycache__"]
+    dir_list = [i for i in os.listdir(".") if not i in ignore_list and os.path.isdir(i)]
+    for dir in dir_list:
+        print(dir)
+
+    print("\nEnter the path the data files are in.\nCan copy and paste from the above list or enter . to select current directory.")
+    directory_name = input("Enter path :")    
+    count = list_all_data_files(directory_name)
+    if count > 0:
+        run_plot_program(directory_name)
+    else:
+        print("No plottable files exist in the directory.")
+    restart = input("Enter r/R to restart:")
+    if restart is not "r" and restart is not "R": break
+    plt.close("all")
 
